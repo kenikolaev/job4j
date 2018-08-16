8e88d29132b250116f43f3f44750d99df7a24a92
kenikolaev <mailto:k.e.nikolaev@gmail.com>
2018-08-16 11:22:27 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,13 +6,13 @@ package ru.job4j.calculate;
 *
 * @author Konstantin Nikolaev (mailto:k.e.nikolaev@gmail.com)
 */
-public class Calculate{
+public class Calculate {
 	
 	/**
 	* Main, вывод строки в консоль.
 	* @param args - args.
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
 		
